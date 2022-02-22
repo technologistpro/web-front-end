7423ffa1af6b9272e4a1f89bef2de14cdd37d493
John Gallo <jgallo@cpg.org>
2022-02-21 21:26:17 -0500

    Javascript based navigation

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,22 +1,20 @@
 var tabs = document.querySelectorAll("nav a");
-var logo = document.querySelector(".logo i");
 
-// Playing a bit with Font Awesome
-if (window.innerWidth < 600) {
-  logo.classList.replace("fa-bullseye", "fa-cubes");
-} else {
-  logo.classList.replace("fa-cubes", "fa-bullseye");
-}
-
-tabs.forEach((tab) => tab.addEventListener("click", makeActive));
-
-function makeActive(event) {
-  makeInactive();
-  event.target.classList.add("active");
-
-  event.preventDefault();
-}
-
-function makeInactive() {
+function makeAllTabsInactive() {
   tabs.forEach((tab) => tab.classList.remove("active"));
 }
+
+function setActiveTabAccordingToHash(currentHash) {
+  makeAllTabsInactive();
+  var tabToActivate = document.querySelector(`a[href="#${currentHash}"]`);
+  tabToActivate.classList.add("active");
+}
+
+function setPageAccordingToHash() {
+  var currentHash = window.location.hash.substring(1);
+  setActiveTabAccordingToHash(currentHash);
+}
+
+window.addEventListener("hashchange", setPageAccordingToHash);
+
+initializePage();
